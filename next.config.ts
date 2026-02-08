84f632034ac3e836f5500eb217cdebe921da6056
mithun jose <mithunjose123@gmail.com>
2026-02-08 13:05:11 +0000

    Try fixing this error: `Runtime Error: Invalid src prop (https://h.crica

--- next.config.ts
+++ next.config.ts
@@ -34,6 +34,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'h.cricapi.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
